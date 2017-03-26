8dcb79994932037c2451db57cc6260c79c8d15f9
刘浩宇 <lhy20010403@hotmail.com>
2017-03-26 12:49:57 +0800

    FirstCommit

---
+++ WoodenBench Mobile/WoodenBench_Mobile/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace WoodenBench_Mobile.Views
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
